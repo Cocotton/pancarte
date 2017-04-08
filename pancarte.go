6bb00777a30bc62912950e16e634e40c75c6a25f
cocotton <julien.parent.trudeau@gmail.com>
2017-04-08 14:51:15 -0400

    Add mongodb support

--- pancarte.go
+++ pancarte.go
@@ -6,29 +6,50 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
 )
 
 type door struct {
 	Address     string `json:"address"`
-	Description string `json:"descrition"`
+	Description string `json:"description"`
 	Price       string `json:"price"`
 	OwnerName   string `json:"ownerName"`
 	OwnerPhone  string `json:"ownerPhone"`
 }
 
-var doors []door
+var session *mgo.Session
 
 func main() {
+	var err error
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/addDoor", addDoor).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
 
 func addDoor(w http.ResponseWriter, r *http.Request) {
+	s := session.Copy()
+	defer s.Close()
+
 	newDoor := new(door)
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newDoor)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	c := s.DB("pancarte").C("doors")
+
+	err = c.Insert(newDoor)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
