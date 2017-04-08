97a5eeb9e920fcf9e9967f73e3f013c7987975e8
cocotton <julien.parent.trudeau@gmail.com>
2017-04-08 13:57:08 -0400

    Add basic addDoor func

--- pancarte.go
+++ pancarte.go
@@ -1,19 +1,34 @@
 package main
 
-type owner struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Phone     string `json:"phone"`
-	Email     string `json:"email"`
-}
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type door struct {
-	Address     string  `json:"address"`
-	Description string  `json:"descrition"`
-	Price       string  `json:"price"`
-	Owners      []Owner `json:"owner"`
+	Address     string `json:"address"`
+	Description string `json:"descrition"`
+	Price       string `json:"price"`
+	OwnerName   string `json:"ownerName"`
+	OwnerPhone  string `json:"ownerPhone"`
 }
 
-func main() {
+var doors []door
 
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/addDoor", addDoor).Methods("POST")
+	http.ListenAndServe(":8080", router)
+}
+
+func addDoor(w http.ResponseWriter, r *http.Request) {
+	newDoor := new(door)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newDoor)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
