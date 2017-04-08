1da376759bb6ab633f5665e2efbdc9e9f398c22b
cocotton <julien.parent.trudeau@gmail.com>
2017-04-08 13:08:16 -0400

    Add default owner struct

---
+++ pancarte.go
@@ -0,0 +1,12 @@
+package main
+
+type owner struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Phone     string `json:"phone"`
+	Email     string `json:"email"`
+}
+
+func main() {
+
+}
