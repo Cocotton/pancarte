a05c2a08666801e00413b2067202b16e67f13815
cocotton <julien.parent.trudeau@gmail.com>
2017-04-08 13:08:38 -0400

    Add default door struct

--- pancarte.go
+++ pancarte.go
@@ -7,6 +7,13 @@ type owner struct {
 	Email     string `json:"email"`
 }
 
+type door struct {
+	Address     string  `json:"address"`
+	Description string  `json:"descrition"`
+	Price       string  `json:"price"`
+	Owners      []Owner `json:"owner"`
+}
+
 func main() {
 
 }
