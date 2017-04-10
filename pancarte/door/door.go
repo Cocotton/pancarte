1f2d42f0e525c7559aad91ddfe9042ab0fe4a7ea
cocotton <julien.parent.trudeau@gmail.com>
2017-04-09 22:10:00 -0400

    Create custom IDs for new doors

--- pancarte/door/door.go
+++ pancarte/door/door.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
@@ -12,6 +13,7 @@ import (
 )
 
 type door struct {
+	ID          string `json:"id"`
 	Address     string `json:"address"`
 	Description string `json:"description"`
 	Price       string `json:"price"`
@@ -25,20 +27,36 @@ func AddDoor(w http.ResponseWriter, r *http.Request, s *mgo.Session) {
 	defer session.Close()
 
 	newDoor := new(door)
+
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newDoor)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c := session.DB("pancarte").C("doors")
+	nextID := getNextID(session)
+	newDoor.ID = nextID
 
+	c := session.DB("pancarte").C("doors")
 	err = c.Insert(newDoor)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func getNextID(s *mgo.Session) string {
+	var result bson.M
+
+	c := s.DB("pancarte").C("counters")
+	change := mgo.Change{
+		Update:    bson.M{"$inc": bson.M{"counter": 1}},
+		ReturnNew: true,
+	}
+	_, _ = c.Find(bson.M{"_id": "doorid"}).Apply(change, &result)
+
+	return strconv.FormatFloat(result["counter"].(float64), 'f', -1, 64)
 }
 
 // GetDoor gets a door from the mongo database using the provided ID
