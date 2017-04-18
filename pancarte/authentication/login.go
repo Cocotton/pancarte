1d875bcd4adebda41c9696d041f91ed05ce6a914
cocotton <julien.parent.trudeau@gmail.com>
2017-04-18 13:46:31 -0400

    Add very basic login password matcher

---
+++ pancarte/authentication/login.go
@@ -0,0 +1,41 @@
+package authentication
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/cocotton/pancarte/pancarte/response"
+	"github.com/cocotton/pancarte/pancarte/user"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type login struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Login makes sure the user exists in the database and that its password match
+func Login(w http.ResponseWriter, r *http.Request, session *mgo.Session) {
+	l := login{}
+	err := json.NewDecoder(r.Body).Decode(&l)
+	if err != nil {
+		log.Fatal("Can't decode user data")
+	}
+
+	fetchedUser := user.User{}
+	c := session.DB("pancarte").C("users")
+	err = c.Find(bson.M{"username": l.Username}).One(&fetchedUser)
+	if err != nil {
+		response.ErrorWithText(w, "Unable to find user: "+l.Username, http.StatusNotFound)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(fetchedUser.Password), []byte(l.Password)); err != nil {
+		response.ErrorWithText(w, "Password does not match", http.StatusForbidden)
+		return
+	}
+	response.SuccessWithJSON(w, []byte("Password matched"), http.StatusOK)
+}
