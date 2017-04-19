316d7864b68361e03a1ad1fdcfe98a68aaa9a2c7
cocotton <julien.parent.trudeau@gmail.com>
2017-04-19 19:23:17 -0400

    Add door.ValidateDoor func

--- door/door.go
+++ door/door.go
@@ -1,5 +1,10 @@
 package door
 
+import (
+	"errors"
+	"reflect"
+)
+
 // Door is a struct containing all the data concerning a Door
 type Door struct {
 	ID          string `json:"id"`
@@ -8,4 +13,16 @@ type Door struct {
 	Price       string `json:"price"`
 	OwnerName   string `json:"ownerName"`
 	OwnerPhone  string `json:"ownerPhone"`
+}
+
+// ValidateDoor makes sure no fields are empty in the Door object, execpt for ID.
+func ValidateDoor(door *Door) error {
+	r := reflect.ValueOf(door).Elem()
+
+	for i := 1; i < r.NumField(); i++ {
+		if r.Field(i).Len() == 0 {
+			return errors.New("Empty field: " + r.Type().Field(i).Name)
+		}
+	}
+	return nil
 }
