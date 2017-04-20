78f7af595c475608fe315ce4880884d8077403f6
cocotton <julien.parent.trudeau@gmail.com>
2017-04-20 10:42:28 -0400

    Add http.ListenAndServe in main

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/cocotton/pancarte/pancarte"
+import (
+	"net/http"
+
+	"github.com/cocotton/pancarte/pancarte"
+)
 
 func main() {
 	p := pancarte.Pancarte{}
 
 	p.InitDB("localhost", "pancarte")
 	p.InitRouter()
+	http.ListenAndServe(":8080", p.Router)
 }
