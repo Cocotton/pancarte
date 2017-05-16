aba99bc678eed02ebc02e5341e05ed6130556a9b
cocotton <julien.parent.trudeau@gmail.com>
2017-05-16 14:15:12 -0400

    Update unit tests

--- location/location_test.go
+++ location/location_test.go
@@ -13,15 +13,15 @@ func TestValidateLocation(t *testing.T) {
 		location    Location
 		expectError bool
 	}{
-		{Location{45.494660, -73.583008, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, false},
-		{Location{*new(float64), -73.583008, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
-		{Location{45.494660, *new(float64), "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "Canada", "", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "Canada", "Qc", "", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "Canada", "Qc", "Montréal", "", "2295", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "", "H3H2G9"}, true},
-		{Location{45.494660, -73.583008, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", ""}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, false},
+		{Location{GeoLocation{"", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{45.494660}}, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "", "Qc", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "", "Montréal", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "", "Rue Saint-Marc", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "Montréal", "", "2295", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "", "H3H2G9"}, true},
+		{Location{GeoLocation{"point", []float64{-73.583008, 45.494660}}, "Canada", "Qc", "Montréal", "Rue Saint-Marc", "2295", ""}, true},
 	}
 
 	for _, test := range validateLocationTests {
